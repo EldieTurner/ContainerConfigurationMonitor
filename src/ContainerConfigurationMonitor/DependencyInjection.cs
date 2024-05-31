075b3f9747270fecc5c4f03fda9bcae00b956e5d
Eldie Turner <Eldie.Turner+git@gmail.com>
2024-05-30 23:35:16 -0400

    updated stuff

--- src/ContainerConfigurationMonitor/DependencyInjection.cs
+++ src/ContainerConfigurationMonitor/DependencyInjection.cs
@@ -22,37 +22,5 @@ namespace ContainerConfigurationMonitor
                                         configFilePath));
             return services;
         }
-
-        public static IDisposable OnChangeDebouncer<TOptions>(this IOptionsMonitor<TOptions> optionsMonitor, Action<TOptions> listener, TimeSpan debounceTime)
-        {
-            Timer debounceTimer = null;
-            TOptions latestOptions = default(TOptions);
-            object lockObj = new object();
-
-            void OnChange(TOptions options)
-            {
-                lock (lockObj)
-                {
-                    latestOptions = options;
-                    if (debounceTimer == null)
-                    {
-                        debounceTimer = new Timer(state =>
-                        {
-                            lock (lockObj)
-                            {
-                                listener(latestOptions);
-                                debounceTimer.Dispose();
-                                debounceTimer = null;
-                            }
-                        }, null, debounceTime, Timeout.InfiniteTimeSpan);
-                    }
-                    else
-                    {
-                        debounceTimer.Change(debounceTime, Timeout.InfiniteTimeSpan);
-                    }
-                }
-            }
-            return optionsMonitor.OnChange(OnChange);
-        }
     }
 }
---
+++ src/ContainerConfigurationMonitor/ExtensionMethods.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Options;
+using System.Threading;
+using System;
+
+namespace ContainerConfigurationMonitor
+{
+    public static  class ExtensionMethods
+    {
+
+        public static IDisposable OnChangeDebouncer<TOptions>(this IOptionsMonitor<TOptions> optionsMonitor, Action<TOptions> listener, TimeSpan debounceTime)
+        {
+            Timer debounceTimer = null;
+            TOptions latestOptions = default(TOptions);
+            object lockObj = new object();
+
+            void OnChange(TOptions options)
+            {
+                lock (lockObj)
+                {
+                    latestOptions = options;
+                    if (debounceTimer == null)
+                    {
+                        debounceTimer = new Timer(state =>
+                        {
+                            lock (lockObj)
+                            {
+                                listener(latestOptions);
+                                debounceTimer.Dispose();
+                                debounceTimer = null;
+                            }
+                        }, null, debounceTime, Timeout.InfiniteTimeSpan);
+                    }
+                    else
+                    {
+                        debounceTimer.Change(debounceTime, Timeout.InfiniteTimeSpan);
+                    }
+                }
+            }
+            return optionsMonitor.OnChange(OnChange);
+        }
+    }
+}
